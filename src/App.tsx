d9d893f4eb7edd8ba0dc9f64dbfaf15b93617d29
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-21 21:59:45 +0100

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 
+import AuthLayout from './_auth/AuthLayout';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
 import './global.css'
+import {Routes , Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <main className='flex gap-2'>
-      <p className="text-3xl font-bold underline">Home</p>
-      <p className="text-3xl font-bold underline">Home</p>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout/>} >
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* Private Routes */}
+
+        <Route element={<RootLayout/>} >
+        <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </main>
   );
 }
