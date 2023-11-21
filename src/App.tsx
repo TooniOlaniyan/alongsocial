9bd7bd7fc8770d6ddc2238474e49da91e470a320
TooniOlaniyan <asalamlatif@gmail.com>
2023-11-21 21:22:55 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './global.css'
+
+function App() {
+
+  return (
+    <main className='flex gap-2'>
+      <p className="text-3xl font-bold underline">Home</p>
+      <p className="text-3xl font-bold underline">Home</p>
+    </main>
+  );
+}
+
+export default App
